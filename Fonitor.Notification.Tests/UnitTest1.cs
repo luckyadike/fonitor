9df1dfae99dedab51b924c8bb57739bc0fb372f6
Lucky Adike <luckyadike@gmail.com>
2014-11-30 14:17:02 -0800

    ImageController Tests.

---
+++ Fonitor.Notification.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Fonitor.Notification.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           Email.SendImageChangeNotification("luckyadike@gmail.com", "Indoor Gym", null);
+        }
+    }
+}
---
+++ Fonitor.Tests/Repositories/InMemoryTableRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace Fonitor.Tests.Repositories
+{
+    using Fonitor.Data.Repositories;
+    using System;
+    using System.Collections.Generic;
+
+    public class InMemoryTableRepository<T> : ITableRepository<T>
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Retrieve(string partitionKey, string rowKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> RetrievePartition(string partitionKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
