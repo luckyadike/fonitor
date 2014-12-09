49c69129453860fe95c09bb422a5e197b62025cf
Lucky Adike <luckyadike@gmail.com>
2014-12-09 03:25:04 -0800

    Revert

--- Fonitor.Data/Repositories/BlobRepository.cs
+++ Fonitor.Data/Repositories/BlobRepository.cs
@@ -5,7 +5,7 @@
 	using System.Collections.Generic;
 	using System.IO;
 
-    public class BlobRepository : IBlobRepository
+    public class BlobRepository
     {
         public BlobRepository(StorageService service)
 		{
