4bca56da826b3070bf056917bdf72b424413cbb7
Lucky Adike <luckyadike@gmail.com>
2014-12-09 03:54:07 -0800

    Revert "Revert "ImageController Tests.""
    This reverts commit 461485fab03016210d0b1e2ecbb8b0b2db33ae0c.

---
+++ Fonitor.Tests/Repositories/InMemoryBlobRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace Fonitor.Tests.Repositories
+{
+    using Fonitor.Data.Repositories;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class InMemoryBlobRepository : IBlobRepository
+    {
+        IDictionary<string, Stream> Store;
+
+        public InMemoryBlobRepository()
+        {
+            Store = new Dictionary<string, Stream>();
+        }
+
+        public void Add(System.IO.Stream entity, string container, string key)
+        {
+            Store.Add(container + key, entity);
+        }
+
+        public void AddWithMetadata(Stream entity, string container, string key, IDictionary<string, string> metadata)
+        {
+            Store.Add(container + key, entity);
+        }
+
+        public void AddOrReplace(Stream entity, string container, string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream RetrieveAsStream(string container, string key)
+        {
+            return Store[container + key];
+        }
+
+        public int Count()
+        {
+            return Store.Count;
+        }
+    }
+}
