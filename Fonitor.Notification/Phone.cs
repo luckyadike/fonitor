09bb034d2e248a563d05468184d79f24568e34e1
Lucky Adike <luckyadike@gmail.com>
2015-05-11 23:43:48 -0700

    SMS Notification class.

---
+++ Fonitor.Notification/Phone.cs
@@ -0,0 +1,38 @@
+﻿namespace Fonitor.Notification
+{
+    using System.Configuration;
+    using System.IO;
+    using Twilio;
+
+    public class Phone
+    {
+        public static void SendImageChangeNotification(string recipientPhoneNumber,
+                                                       string sensorName,
+                                                       MemoryStream changedImageStream)
+        {
+            // Retrieve settings.
+            // Ensure that they are set?
+            var sid = ConfigurationManager.AppSettings["TwilioAccountSID"];
+
+            var token = ConfigurationManager.AppSettings["TwilioAuthToken"];
+
+            var senderPhone = ConfigurationManager.AppSettings["PhoneNumber"];
+
+            var client = new TwilioRestClient(sid, token);
+
+            var message = string.Format("{0} has detected a problem! Please review the attached image and take appropriate action.", sensorName);
+
+            var result = client.SendMessage(
+                senderPhone,
+                recipientPhoneNumber,
+                message);
+
+            if (result.RestException != null)
+            {
+                // an exception occurred making the REST call
+                // Log this message.
+                string error = result.RestException.Message;
+            }
+        }
+    }
+}
